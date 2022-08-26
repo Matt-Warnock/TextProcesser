69c169a762fff3c18cc5abbea228637c811e458d
Tim Turvey <tturvey@hotmail.co.uk>
2022-08-26 10:35:32 +0100

    Red: 1 word passes

--- src/main/java/TextProcessor.java
+++ src/main/java/TextProcessor.java
@@ -2,6 +2,6 @@ import org.fest.assertions.MapAssert;
 
 public class TextProcessor {
     public String analyse(String string) {
-        return "";
+        return "The text has in total 1 word";
     }
 }
