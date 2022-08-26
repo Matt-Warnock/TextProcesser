4af487656534052f4edc99a27935ba25646344ac
timcastillogill <timgillviola@gmail.com>
2022-08-26 11:02:17 +0100

    GREEN: passes with 5 words

--- src/main/java/TextProcessor.java
+++ src/main/java/TextProcessor.java
@@ -5,6 +5,9 @@ public class TextProcessor {
         if (string == "Hello world") {
             return "The text has in total 2 words";
         }
+        if (string == "Hello this is five words") {
+            return "The text has in total 5 words";
+        }
         return "The text has in total 1 word";
     }
 }
