2e073da90065b582808e8534781c587801c059d3
MattW <mattnwarnock@yahoo.co.uk>
2022-08-26 11:44:52 +0100

    Refactor: word lengt

--- src/main/java/TextProcessor.java
+++ src/main/java/TextProcessor.java
@@ -1,20 +1,15 @@
 public class TextProcessor {
     public String analyse(String string) {
-        int amountOfWords = 1;
-        if (string == "Hello world") {
-            amountOfWords = 2;
-        }
-        if (string == "Hello this is five words") {
-            amountOfWords = 5;
-        }
-        return "The text has in total " + amountOfWords + pluraliseWord(amountOfWords);
+        int amountOfWords = string.split(" ").length;
+
+        return formatWordCount(amountOfWords);
+    }
+
+    private static String formatWordCount(int amountOfWords) {
+        return "The text has in total " + amountOfWords + " " + pluraliseWord(amountOfWords);
     }
 
     private static String pluraliseWord(int amountOfWords) {
-        String pluraliseWord = " words";
-        if (amountOfWords == 1) {
-            pluraliseWord = " word";
-        }
-        return pluraliseWord;
+        return amountOfWords != 1 ? "words" : "word";
     }
 }
